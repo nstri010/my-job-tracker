81991264a4da4bd63ca4836fee7d01bdba1bb386
nstri010 <nstri010@FIU.edu>
2026-05-19 15:13:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import streamlit as s
+import streamlit as st
 from storage import load_jobs, save_job, delete_job, sign_up_user, login_user, upload_resume
 from utils import scrape_job_link, clean_description_with_ai, get_ai_match_feedback, extract_text_from_upload
 
