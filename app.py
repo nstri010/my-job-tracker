6515de81484be857a57458d91e4828e1d821427b
nstri010 <nstri010@FIU.edu>
2026-05-26 02:30:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ if not st.session_state["logged_in"]:
             tab = st.session_state["login_tab"]
             
             if tab == "login":
-                st.markdown("<h2 style='font-size:36px; margin-bottom:8px;'>Welcome Back!</h2>", unsafe_allow_html=True)
+                st.markdown("<h2 style='font-size:36px; margin-bottom:8px;'>Welcome Back</h2>", unsafe_allow_html=True)
                 st.markdown("<p style='margin-bottom:32px;'>Sign in to access your dashboard</p>", unsafe_allow_html=True)
                 
                 u = st.text_input("Username", key="login_username")
